a7018686c255b2999dfe2d4aaac7ede01e096692
dreamer.dead <dreamer.dead@gmail.com>
2014-08-11 20:44:07 +0400

    Add simple JUnit test.

---
+++ src/test/java/ExpressionCalculatorTest.java
@@ -0,0 +1,13 @@
+package org.dreamer.expression.calc;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExpressionCalculatorTest {
+	public ExpressionCalculatorTest() {}
+
+	@Test
+	public void firstTest() {
+		assertEquals(1, 1);
+	}
+}
